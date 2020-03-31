c231f445e12a48c80ef7e1aceeea165ba2be113a
Xvn Anony <62823140+XvnAnony@users.noreply.github.com>
2020-03-31 11:57:25 +0700

    Add files via upload

---
+++ X-Network.sh
@@ -0,0 +1,115 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+echo $red
+figlet "Loading..."
+echo "Berani recode besok mati"
+sleep 5
+clear
+echo $green
+figlet "X Network"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"Author : Xvn Anony"
+echo $white"Github : https://github.com/XvnAnony"
+echo $white"Youtube : Xvn Anony"
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $white"     SELAMAT DATANG BRO"
+echo
+echo $red"Silahkan Dipilih Gratis kok :"
+echo
+echo $green"1.) Jaringan Stabil v1"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"2.) jaringan Stabil v2"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"3.) Jaringan Stabil Saat Browsing"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"4.) Jaringan Stabil Saat nonton Youtube v1"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green"5.) Jaringan Stabil Saat nonton Youtube v2"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $red"6.) Coming Soon...."
+echo $green"<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $red"0.) Exit"
+echo "<{≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈}>"
+echo $green
+read -p "Silahkan Pilih No Berapa : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay X Network
+echo $green"Stabilkan Jaringan Xvn Anony"
+sleep 1
+echo
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay X Network
+echo $yellow"Stabilkan Jaringan v2 Xvn Anony"
+sleep 1
+echo
+ping -s1000 8.8.8.8
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $green
+figlet "Xvn"
+echo $white"Semoga Browsing mu Lancar"
+sleep 1
+echo
+ping -s1000 216.239.38.120
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo $green
+figlet "Y T v 1"
+echo "No Bufering Saat nonton Youtube v1 Xvn Anony"
+sleep 2
+echo
+ping -s1000 74.125.24.91
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo $red
+figlet "Y T v 2"
+echo "jaringan Stabil saat nonton Youtube v2 by Yadi"
+sleep 2
+echo
+ping -s1000 172.217.194.113
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+echo $red
+figlet "Coming soon..."
+echo "Tunggu kehadiran script baru di Github Xvn Anony"
+sleep 2
+exit
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $red
+figlet "Bye"
+echo "Makasih Bro Udah Mampir"
+sleep 2
+exit
+fi
